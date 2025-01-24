4fff97f4d8ea451a9e5efed6a119b83b9fb58d45
AHMAD-FB <124088270+AHMAD-FB@users.noreply.github.com>
2025-01-25 01:11:48 +0300

    Add files via upload

---
+++ Download-instagram.py
@@ -0,0 +1,15 @@
+import requests
+import os
+import telebot
+token = input(" << Enter Token telegram >> : ")
+bot = telebot.TeleBot(token)
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(message.chat.id,f"Hello\n Wellcome To instagram Download Bot : video and Reels and photo  and Tv :  \nSend Link  Now 📎✅ \nCH  : @Abfullah_Abd_alsalam")
+@bot.message_handler(func=lambda m:True)
+def send(message):
+    bot.send_message(message.chat.id,f"Wait Please",parse_mode="html") 
+    url = requests.get(f"https://mr-abood.herokuapp.com/Get/Video/Insta?Link={message.text}").json()
+    io = url["link"]
+    bot.send_video(message.chat.id,io,caption="Done ✅💗 << @Abdullah_Abd_alsalam >> ")
+bot.polling()
